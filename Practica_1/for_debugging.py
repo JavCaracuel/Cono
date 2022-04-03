09d26e9082e11b78911b50d5c7dbc5c91513de21
Javier <javcaracuel1@gmail.com>
2022-04-03 19:52:04 +0200

    profundidad heurística

--- Practica_1/for_debugging.py
+++ Practica_1/for_debugging.py
@@ -1,5 +1,4 @@
 # %%
-from joblib import PrintTime
 import numpy
 import random
 
@@ -9,6 +8,8 @@ import random
 # %%
 different_solutions=[]
 movements=[(1,2),(2,1),(2,-1),(1,-2),(-1,-2),(-2,-1),(-2,1),(-1,2)]
+
+movements_order=[]
 
 def get_coord(number, matrix):
     for i in range(len(matrix)):
@@ -21,24 +22,64 @@ def get_move(val_pos, n, chessboard):
     if (val_pos[0] >= 0) and (val_pos[0] < n) and (val_pos[1] >= 0) and (val_pos[1] < n) and (chessboard[val_pos[0], val_pos[1]] == 0):
         return True # Se devuelve True si el movimiento se encuentra dentro del tablero y no se encuentra ocupado
     return False
-# %%
 
-# %%
+# functions that returns number of posible moves for knight
+def get_moves_number(val_pos, n, chessboard):
+    moves = 0
+    if get_move((val_pos[0] + 2, val_pos[1] + 1), n, chessboard):
+        moves += 1
+    if get_move((val_pos[0] + 2, val_pos[1] - 1), n, chessboard):
+        moves += 1
+    if get_move((val_pos[0] - 2, val_pos[1] + 1), n, chessboard):
+        moves += 1
+    if get_move((val_pos[0] - 2, val_pos[1] - 1), n, chessboard):
+        moves += 1
+    if get_move((val_pos[0] + 1, val_pos[1] + 2), n, chessboard):
+        moves += 1
+    if get_move((val_pos[0] + 1, val_pos[1] - 2), n, chessboard):
+        moves += 1
+    if get_move((val_pos[0] - 1, val_pos[1] + 2), n, chessboard):
+        moves += 1
+    if get_move((val_pos[0] - 1, val_pos[1] - 2), n, chessboard):
+        moves += 1
+    return moves
+
+#function bubble sort
+def bubble_sort(lista):
+    for i in range(len(lista)-1,0,-1):
+        for j in range(i):
+            if lista[j][0]>lista[j+1][0]:
+                aux=lista[j]
+                lista[j]=lista[j+1]
+                lista[j+1]=aux
+    return lista
+
+
 def knights_tour(posicion,n,move_number,chessboard):
-
+    global movements_order
+    movements_order = []
     if move_number>n*n:
         different_solutions.append(chessboard.copy())
         print(f"\rnum sol = {len(different_solutions)}", end="")
-        x,y = get_coord(move_number-1, chessboard)
-        chessboard[x,y] = 0
+        # descomentar esto si queremos mas de una solucion
+        # x,y = get_coord(move_number-1, chessboard)
+        # chessboard[x,y] = 0
         return True
 
     for i in movements:
         x,y = get_coord(move_number-1, chessboard)
         posicion=(x+i[0],y+i[1])
-        if get_move(posicion, n, chessboard):
-            chessboard[posicion[0],posicion[1]] = move_number
-            knights_tour(posicion,n,move_number+1,chessboard)
+        if get_move(posicion,n,chessboard):
+            num_valores_aux=get_moves_number(posicion,n,chessboard)
+            movements_order.append((num_valores_aux,posicion))
+        
+    movements_order= bubble_sort(movements_order)
+
+    for i in movements_order:
+        chessboard[i[1][0],i[1][1]] = move_number
+        knights_tour(i[1],n,move_number+1,chessboard)
+        # comentar este break si queremos más de 1 solucion
+        break
             
     x,y = get_coord(move_number-1, chessboard)
     chessboard[x,y] = 0
@@ -47,16 +88,34 @@ def knights_tour(posicion,n,move_number,chessboard):
 
 
 def main():
+    intro=0
     move_number=1
-    n = int(input("Ingrese el tamaño de la matriz: "))
-    chessboard = numpy.zeros((n, n))
-    posicion=(0,0)
+    while intro == 0:
+        print("Elija el tamaño del tablero:")
+        size_tablero=input()
+        # Control de errores
+        if size_tablero.isnumeric():
+            intro=1
+            size_tablero= int(size_tablero)
+        else:
+            print("Valor no válido")
+        print("Elija la posición inicial del caballo con la forma [x,y]:")
+        posicion=input()
+        # Control de errores
+        posicion=posicion.split(",")
+        try:
+            posicion=(int(posicion[0]),int(posicion[1]))
+            posicion= (posicion[0],posicion[1])
+            intro=1
+        except:
+            print("Valor no válido")
+            intro=0
+
+    chessboard = numpy.zeros((size_tablero, size_tablero))
     chessboard[posicion[0],posicion[1]] = move_number
     move_number+=1
-   
 
-
-    knights_tour(posicion,n,move_number,chessboard)
+    knights_tour(posicion,size_tablero,move_number,chessboard)
     for i,j in enumerate(different_solutions):
         print(f"num sol = {i}")
         print(j)
